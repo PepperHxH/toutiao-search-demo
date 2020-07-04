9ef3c44a7dd79495d9f4e1fd736715687c072200
PepperHxH <838245976@qq.com>
2020-07-04 18:08:36 +0800

    feat:添加搜索框

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Input } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Search } = Input;
+
+
+class App extends React.Component {
+
+  handle = (value) => {
+    console.log(value);
+  }
+  
+  render() {
+    return (
+      <div className='content'>
+        <Search
+          placeholder="请输入搜索内容"
+          enterButton="搜索"
+          size="large"
+          onSearch={(value) => { this.handle(value); }}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
