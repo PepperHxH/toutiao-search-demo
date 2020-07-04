6c1d390c5dee64b73e0f8d8840eb4c76b852b1b8
PepperHxH <838245976@qq.com>
2020-07-04 18:19:08 +0800

    feat:修改handle事件

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './App.css';
 import { Input } from 'antd';
+import { Link } from "react-router-dom";
 
 const { Search } = Input;
 
-
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    }
+  }
+
   handle = (value) => {
+    this.setState({
+      value: value
+    });
     console.log(value);
   }
-  
+
   render() {
     return (
       <div className='content'>
